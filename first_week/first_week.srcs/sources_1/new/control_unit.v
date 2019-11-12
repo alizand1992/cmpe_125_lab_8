25a1a53a421f04928a6eb8adc08ff848fb9b729d
damienlevitt <damien.levitt@sjsu.edu>
2019-11-11 17:26:54 -0800

    Control Unit stuff

--- first_week/first_week.srcs/sources_1/new/control_unit.v
+++ first_week/first_week.srcs/sources_1/new/control_unit.v
@@ -1,26 +1,22 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/11/2019 04:00:26 PM
-// Design Name: 
-// Module Name: control_unit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module control_unit(
-
+        input clk, rst, go,
+        input [3:0] status_signal,
+        output reg  done, err,
+        output reg [3:0] control_signal
     );
+    
+    always @ (posedge clk)
+    begin
+        if(!rst && go)
+        begin
+            control_signal = control_signal + status_signal;
+    
+            if (control_signal == 4'b0111)
+                { done, err } = 2'b10;
+            else
+                done = 0;
+        end   
+        else
+            control_signal = 0;
+    end
 endmodule
