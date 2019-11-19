83250c159d2bc2a9f8a0ae7bc150918c89a195f8
Ali Zand <ali.zand@sjsu.edu>
2019-11-18 21:56:48 -0800

    Finish constraint file

--- first_week/first_week.srcs/sources_1/new/cudp_fpga.v
+++ first_week/first_week.srcs/sources_1/new/cudp_fpga.v
@@ -2,14 +2,15 @@ module cudp_fpga(
     input clk_btn, clk100MHz, rst_btn, go_sw,
     input [3:0] a, b,
     
-    output [7:0] q_led_l, q_led_r, r_led_l, r_led_r, LEDOUT,
+    output [7:0] LEDOUT,
     output [3:0] LEDSEL, 
     output err, done
 );
     
     wire clk, clk_5KHz, rst, DONT_USE;
     wire [3:0] q, r;
-    
+    wire [7:0] q_led_l, q_led_r, r_led_l, r_led_r;
+        
     cudp calc (
         .dividend(a), .divisor(b), .go(go_sw), .clk(clk), .rst(rst),
         .quotient(q), .remainder(r), .err(err), .done(done) 
