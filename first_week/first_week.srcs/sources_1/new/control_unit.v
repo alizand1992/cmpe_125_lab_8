349cf7608922cc8e603471d90f8b8907c080e426
Ali Zand <ali.zand@sjsu.edu>
2019-11-18 21:08:36 -0800

    Update control_unit

--- first_week/first_week.srcs/sources_1/new/control_unit.v
+++ first_week/first_week.srcs/sources_1/new/control_unit.v
@@ -1,6 +1,6 @@
 module control_unit(
         input clk, rst, go,
-        input [3:0] status_signal,
+        input [4:0] status_signal,
         output reg  done, err,
         output reg [3:0] control_signal
     );
@@ -9,12 +9,32 @@ module control_unit(
     begin
         if(!rst && go)
         begin
-            control_signal = control_signal + status_signal;
-    
-            if (control_signal == 4'b0111)
-                { done, err } = 2'b10;
-            else
-                done = 0;
+            case(status_signal)
+                0: //idle
+                begin
+                    control_signal <= 1;
+                    err <= 0;
+                    done <= 0;
+                end
+                1: // error
+                begin
+                    control_signal <= 0;
+                    err <= 1;
+                    done <= 0;
+                end
+                2: // status ok go to next state
+                    control_signal = control_signal + 1;
+                3: // remainder less than divisor go to state 4
+                    control_signal <= 4;
+                4: // remainder more than divisor go to state 4
+                    control_signal <= 5;
+                5: //  done wit division go to 6
+                    control_signal <= 6;
+                6: // still dividing go to 3
+                    control_signal <= 3;
+                7: // done with everything go to 1
+                    control_signal <= 1;
+            endcase
         end   
         else
             control_signal = 0;
